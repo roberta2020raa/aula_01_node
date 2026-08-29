679387eba41d7bce1b05e43e1b94b08f01a1614b
roberta2020raa <roberta2020.raa@gmail.com>
2026-08-29 09:54:55 -0300

    cria o primeiro servidor

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # aula_01_node
 aula 00 de node-backend em javaScript
+# terminal bloqueando
+'''
+Set-ExecutionPolicy -ExecutionPolicy Bypass -scope Process 
+'''
+# instalar bibliotecas
+'''
+npm install express
+npm install --save-dev nodemon
+npm instal dotenv 
+
+# comando básicos 
+git add .
+git commit -m "mensagem"
+git push
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Olá mundo!")
+import express from 'express'
+
+const app = express()
+
+//função callback
+app.get('/busca', (req, res) => {
+    console.log(req)
+  res.send('Hello World')
+})
+   
+  
+  
+
+app.get('/busca/:id', (req, res) => {
+    const numero =req.params.id
+    console.log(numero)
+    const mensagem = `o numero requisitado é:${numero}`
+    res.send(mensagem)
+})
+
+
+ app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
+   
